b783bc474a5a8eaeedcbe5ab4596a792c7ad94eb
SarahRubio <sarahrubio.pro@gmail.com>
2021-10-24 19:19:09 +0200

    create search form

---
+++ src/objects/forms.py
@@ -0,0 +1,134 @@
+import re
+import operator
+
+from django import forms
+from django.db.models import Q, F
+from django.core.exceptions import ValidationError
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.postgres.search import SearchQuery, SearchRank
+
+from filters.models import Category, SubCategory, Color, Designer, Material, State, Style
+
+
+class ObjectSearchForm(forms.Form):
+    """Main form for search engine."""
+
+    SUBCATEGORIES_QS = SubCategory.objects \
+        .select_related('category') \
+        .order_by('category__name', 'name')
+
+    categories = forms.ModelMultipleChoiceField(
+        label='Catégories',
+        queryset=Category.objects.all(),
+        to_field_name='slug',
+        required=False,
+        widget=forms.CheckboxSelectMultiple)
+    subcategories = forms.ModelMultipleChoiceField(
+        label='Catégories',  # Not a mistake
+        queryset=SUBCATEGORIES_QS,
+        to_field_name='slug',
+        required=False,
+         widget=forms.CheckboxSelectMultiple)
+    is_negotiable = forms.BooleanField(
+        label='prix négociable',
+        required=False)
+    price_min = forms.IntegerField(
+        label='min',
+        required=False)
+    price_max = forms.IntegerField(
+        label='max',
+        required=False)
+    designers = forms.ModelMultipleChoiceField(
+        label="Designer",
+        queryset=Designer.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple)
+    state = forms.ModelMultipleChoiceField(
+        label="état",
+        queryset=State.objects.all(),
+        to_field_name='slug',
+        required=False,
+        widget=forms.CheckboxSelectMultiple)
+    materials = forms.ModelMultipleChoiceField(
+        label="Matériaux",
+        queryset=Material.objects.all(),
+        to_field_name='slug',
+        required=False,
+        widget=forms.CheckboxSelectMultiple)
+    style = forms.ModelMultipleChoiceField(
+        label="Style",
+        queryset=Style.objects.all(),
+        to_field_name='slug',
+        required=False,
+        widget=forms.CheckboxSelectMultiple)
+    colors = forms.ModelMultipleChoiceField(
+        label='Couleurs',
+        required=False,
+        queryset=Color.objects.all(),
+        widget=forms.CheckboxSelectMultiple)
+
+    def filter_queryset(self, qs=None):
+        """Filter querysets depending of input data."""
+
+        # If no qs was passed, just start with all published aids
+        if qs is None:
+            qs = Light.objects.all()
+
+        if not self.is_bound:
+            return qs
+
+        # Populate cleaned_data
+        if not hasattr(self, 'cleaned_data'):
+            self.full_clean()
+
+        is_negotiable= self.cleaned_data.get('is_negotiable', False)
+        if is_negotiable:
+            qs = qs.filter(is_negotiable=True)
+
+        subcategories = self.cleaned_data.get('subcategories', None)
+        if subcategories:
+            qs = qs.filter(subcategories__in=subcategories)
+
+        designers = self.cleaned_data.get('designers', None)
+        if designers:
+            qs = qs.filter(designer__in=designers)
+
+        materials = self.cleaned_data.get('materials', None)
+        if materials:
+            qs = qs.filter(materials__in=materials)
+
+        price_min = self.cleaned_data.get('price_min', 0)
+        price_max = self.cleaned_data.get('price_max', None)
+        if price_min or price_max:
+            if not price_min:
+                self.cleaned_data['price_min'] = 0
+                price_min = self.cleaned_data['price_min']
+                qs = qs.filter(price__range=(price_min, price_max))
+            if not price_max:
+                self.cleaned_data['price_max'] = 999999
+                price_max = self.cleaned_data['price_max']
+                qs = qs.filter(price__range=(price_min, price_max))
+            else:
+                qs = qs.filter(price__range=(price_min, price_max))
+
+        state = self.cleaned_data.get('state', None)
+        if state:
+            qs = qs.filter(state__in=state)
+
+        style = self.cleaned_data.get('style', None)
+        if style:
+            qs = qs.filter(style__in=style)
+
+        colors = self.cleaned_data.get('colors', None)
+        if colors:
+            qs = qs.filter(colors__in=colors)
+
+        # We filter by theme only if no categories were provided.
+        # This is to handle the following edge case: on the multi-step search
+        # form, the user selects a category, then on the last step, doesn't select
+        # any subcategories and just click "Search".
+        categories = self.cleaned_data.get('categories', None)
+        if categories and not subcategories:
+            qs = qs.filter(subcategories__categories__in=categories)
+
+        return qs
--- src/objects/views.py
+++ src/objects/views.py
@@ -1,18 +1,70 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
 from objects.models import Light, Finding, Furniture
+from objects.forms import ObjectSearchForm
 
 
-class LightList(ListView):
+class SearchView(FormMixin, ListView):
+    """Search and display objects."""
+
+    form_class = ObjectSearchForm
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_form()
+        self.form.full_clean()
+        self.store_current_search()
+        return super().get(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        """Take input data from the GET values."""
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'data': self.request.GET,
+        })
+        return kwargs
+
+    def store_current_search(self):
+        """Store the current search query in a cookie.
+
+        This is needed to provide the correct "go back to your search" link in
+        other pages' breadcrumbs.
+        """
+        current_search_query = self.request.GET.urlencode()
+        self.request.session['SEARCH_COOKIE_NAME'] = current_search_query
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['current_search'] = self.request.session.get('SEARCH_COOKIE_NAME', '')
+
+        return context
+
+
+class LightList(SearchView):
     template_name = 'lights/lights_list.html'
     context_object_name = 'lights'
     paginate_by = 18
+    form_class = ObjectSearchForm
 
     def get_queryset(self):
-        qs = Light.objects.all()
-        return qs
+        """Return the list of results to display."""
 
+        qs = Light.objects \
+            .prefetch_related('designer') \
+            .prefetch_related('categories') \
+            .prefetch_related('subcategories') \
+            .prefetch_related('style') \
+            .prefetch_related('materials') \
+            .prefetch_related('colors') \
+            .prefetch_related('state')
+
+        filter_form = self.form
+        results = filter_form.filter_queryset(qs).distinct()
+
+        return results
+    
 
 class LightDetail(DetailView):
     template_name = 'lights/lights_detail.html'
