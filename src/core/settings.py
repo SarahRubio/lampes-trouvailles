2aa4bddfa30795a95a133035f1a8744fe9b29aae
SarahRubio <sarahrubio.pro@gmail.com>
2021-12-18 18:10:38 +0100

    add lampes-trouvailles to allowed-host

--- src/core/settings.py
+++ src/core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.lampes-trouvailles.com']
 
 ENABLE_DJANGO_STATIC_SERVE = False
 
